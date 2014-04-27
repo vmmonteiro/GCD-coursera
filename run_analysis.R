5d0427733cc96bb891734d08becdbe87062812f2
vmmonteiro <d2012167@isegi.unl.pt>
2014-04-27 23:27:40 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,4 @@
+# Peer Assessment for Getting and Cleaning Data
+# Data available at from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# Put the zip file into your working folder and unzip it
+# You will have a folder called "UCI HAR Dataset" where you can find the files for the exercise
